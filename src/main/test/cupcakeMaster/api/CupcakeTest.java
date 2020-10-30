5b58290c25c113e9fffbf9162bf791be4be76dc6
jensgelbek <jensgelbek@gmail.com>
2020-10-30 12:09:42 +0100

    nu med commit og gte af custiomer, sql til DB har nu salt og secret.

--- src/main/test/cupcakeMaster/api/CupcakeTest.java
+++ src/main/test/cupcakeMaster/api/CupcakeTest.java
@@ -4,7 +4,7 @@ import cupcakeMaster.domain.order.DBException;
 import cupcakeMaster.domain.order.customer.Customer;
 import cupcakeMaster.domain.order.customer.CustomerNotFoundException;
 import cupcakeMaster.infrastructure.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 public class CupcakeTest {
 
     @Test
-    public void gemcustomer() throws DBException, SQLException, CustomerNotFoundException {
+   /* public void gemcustomer() throws DBException, SQLException, CustomerNotFoundException {
         Database db = new Database();
         Cupcake cupcake= new Cupcake(new DBOrdreLinieRepository(db), new DBBottomRepository(db), new DBTopRepository(db),new DBCustomerRepository(db));
         Customer customer=new Customer("test@gamil.com",0,true,"123455".getBytes(),"fiugi".getBytes());
@@ -20,6 +20,6 @@ public class CupcakeTest {
         Customer newcustomer =cupcake.findCustomer("test@gamil.com");
         System.out.println(newcustomer.getCustomerId());
 
-    }
+    }*/
   
 }
